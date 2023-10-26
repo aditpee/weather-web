945a4c0e76c67de6478a7d8a7f2948d735354b15
aditpee <p254881@gmail.com>
2023-10-26 18:38:52 +0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const {
 } = require("./utils/weather");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
