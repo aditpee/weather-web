06e5cc323923f08645944f0f840478430b629ab4
aditpee <p254881@gmail.com>
2023-10-28 13:34:32 +0700

    test load wit out geolocation api

--- public/script/getLocation.js
+++ public/script/getLocation.js
@@ -1,35 +1,35 @@
 // ===== get your location coordinate ====
-const currentBtn = document.getElementById("current-weather");
-const currentLat = document.getElementById("current-lat");
-const currentLon = document.getElementById("current-lon");
+// const currentBtn = document.getElementById("current-weather");
+// const currentLat = document.getElementById("current-lat");
+// const currentLon = document.getElementById("current-lon");
 
-if (!navigator.geolocation) {
-  if (currentLat.value == "") {
-    currentLat.value = "51.5073219";
-    currentLon.value = "-0.1276474";
-  }
-}
+// if (!navigator.geolocation) {
+//   if (currentLat.value == "") {
+//     currentLat.value = "51.5073219";
+//     currentLon.value = "-0.1276474";
+//   }
+// }
 
-function success(pos) {
-  const crd = pos.coords;
-  currentLat.value = crd.latitude ? crd.latitude : "51.5073219";
-  currentLon.value = crd.longitude ? crd.longitude : "-0.1276474";
+// function success(pos) {
+//   const crd = pos.coords;
+//   currentLat.value = crd.latitude ? crd.latitude : "51.5073219";
+//   currentLon.value = crd.longitude ? crd.longitude : "-0.1276474";
 
-  currentBtn.click();
-}
-function getDefaultLocation() {
-  if (currentLat.value == "") {
-    currentLat.value = "51.5073219";
-    currentLon.value = "-0.1276474";
-  }
-  currentBtn.click();
-  alert("please activate your location to get weather at your location");
-}
+//   currentBtn.click();
+// }
+// function getDefaultLocation() {
+//   if (currentLat.value == "") {
+//     currentLat.value = "51.5073219";
+//     currentLon.value = "-0.1276474";
+//   }
+//   currentBtn.click();
+//   alert("please activate your location to get weather at your location");
+// }
 
-function getYourLocation() {
-  navigator.geolocation.getCurrentPosition(success, getDefaultLocation, {
-    enableHighAccuracy: false,
-  });
-}
+// function getYourLocation() {
+//   navigator.geolocation.getCurrentPosition(success, getDefaultLocation, {
+//     enableHighAccuracy: false,
+//   });
+// }
 
-export { getYourLocation };
+// export { getYourLocation };
